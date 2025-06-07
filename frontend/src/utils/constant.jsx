43e0284cf76b24dcf83e200be2206a9b78a584fd
Sreejeshkrish04 <cjsreejeshkrishna@gmail.com>
2025-06-08 03:10:07 +0530

    url updated

--- frontend/src/utils/constant.jsx
+++ frontend/src/utils/constant.jsx
@@ -1,6 +1,9 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const USER_API_END_POINT =
+  "http://localhost:8000https://jobportal-mern-v01m.onrender.com/api/v1/user";
 //export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const JOB_API_END_POINT =
+  "https://jobportal-mern-v01m.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+  "https://jobportal-mern-v01m.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://jobportal-mern-v01m.onrender.com/api/v1/company";
