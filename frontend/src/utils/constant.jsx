f656f37e08e25d54b4e52e336f5846fb26c84ce1
Sreejeshkrish04 <cjsreejeshkrishna@gmail.com>
2025-06-08 04:32:13 +0530

    Fix:npm degraded then url error in constant

--- frontend/src/utils/constant.jsx
+++ frontend/src/utils/constant.jsx
@@ -1,5 +1,5 @@
 export const USER_API_END_POINT =
-  "http://localhost:8000https://jobportal-mern-v01m.onrender.com/api/v1/user";
+  "https://jobportal-mern-v01m.onrender.com/api/v1/user";
 //export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
 export const JOB_API_END_POINT =
   "https://jobportal-mern-v01m.onrender.com/api/v1/job";
