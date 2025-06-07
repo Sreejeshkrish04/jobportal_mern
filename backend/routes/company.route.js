0363d01d3a48f7d0ce1b33da6f24f1bc2c7ae22e
Sreejeshkrish04 <cjsreejeshkrishna@gmail.com>
2025-06-08 03:34:52 +0530

    Fix: cleaned up route syntax

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -16,3 +16,22 @@ router.route("/get/:id").get(isAuthenticated, getCompanyById);
 router.route("/update/:id").put(isAuthenticated, singleUpload, updateCompany);
 
 export default router;
+
+// import express from "express";
+// import {
+//   getCompany,
+//   registerCompany,
+//   getCompanyById,
+//   updateCompany,
+// } from "../controllers/company.controller.js";
+// import isAuthenticated from "../middlewares/isAuthenticated.js";
+// import { singleUpload } from "../middlewares/multer.js";
+
+// const router = express.Router();
+
+// router.route("/register").post(isAuthenticated, registerCompany);
+// router.route("/get").get(isAuthenticated, getCompany);
+// router.route("/get/:id").get(isAuthenticated, getCompanyById);
+// router.route("/update/:id").put(isAuthenticated, singleUpload, updateCompany);
+
+// export default router;
